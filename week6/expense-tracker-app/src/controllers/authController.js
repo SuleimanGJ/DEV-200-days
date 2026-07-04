b07fb7bd4f1e60e95cea3f87e481b4f15a03a1d8
suleiman <suleimanosman1023@gmail.com>
2026-07-04 11:02:12 +0300

    added hashing password

--- week6/expense-tracker-app/src/controllers/authController.js
+++ week6/expense-tracker-app/src/controllers/authController.js
@@ -9,7 +9,7 @@ const register = async (req, res) => {
     await userModel.create({
         username,
         email,
-        hashedPassword
+        password: hashedPassword
     });
     res.json({message: "User registered successfully"})
 };
@@ -17,9 +17,14 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
     const { email, password } = req.body;
-    const userExists = userModel.findOne({email, password});
+    const userExists = userModel.findOne({email});
     if(!userExists){
         res.status(401).json({message: "User doesnot exists"})
+    }
+
+    const isMatch = await bcrypt.compare(password, userExists.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
     }
 
     const token = jwt.sign({id: userExists._id}, SECRET);
