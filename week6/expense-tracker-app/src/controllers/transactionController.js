d93ff05a35be65faf41ef9b2374c35f80162e3fc
suleiman <suleimanosman1023@gmail.com>
2026-07-04 11:11:49 +0300

    added crud transactions functionality

--- week6/expense-tracker-app/src/controllers/transactionController.js
+++ week6/expense-tracker-app/src/controllers/transactionController.js
@@ -9,54 +9,82 @@ import {categoryModel} from "../models/Category.js"
     //     DELETE /transactions/:id
 
 const getAllTransactions = async (req, res) => {
+  const userId = req.userId;
     const transactions = await transactionModel
-      .find({ userId: req.userId })
+      .find({ userId })
       .populate("categoryId", "name type")
-      .sort({ date: -1 });
-    //   .sort({ createdAt: -1 });
-    //   .sort({date: -1}) // sort in desc 
-    //   .sort({createdAt: 1}) // sort in asc
-    res.json({transactions});
+      .sort({ createdAt: -1 });
+
+    res.json({
+      message: "Transactions fetched successfully",transactions
+    });
 }
+
+
 const getTransactionById = async (req, res) => {
-    const id = req.params.id;
+  const userId = req.userId;
+  const transactionId = req.params.id;
   const transaction = await transactionModel.findOne({
-    _id: id,
-    userId: req.userId,
+    _id: transactionId,
+    userId: userId,
   });
   if(!transaction){
     return res.status(404).json({message: "You have no access"})
   }
-  res.json({ transaction });
+  res.json({ 
+    message: "Transaction fetched successfully",
+    transaction
+  });
 };
 
 const createTransaction = async (req, res) => {
     const userId = req.userId;
-    const category = await categoryModel.findOne({ userId: req.userId });
-    if(!categories){
+    const { categoryId } = req.body;
+    const category = await categoryModel.findOne({ 
+    _id: categoryId,
+    userId: userId,
+    });
+    if(!category){
         return res.status(404).json({message: "Catrgory not found or not allowed"})
     }
-    const {title, amount, type, note} = req.body;
-    type = category.type;
-    const newTransaction = await transactionModel.create({name, type, userId});
+    const {title, amount, note} = req.body;
+    
+    const newTransaction = await transactionModel.create({
+    title,
+    amount,
+    type: category.type,
+    note,
+    userId,
+    categoryId,
+    });
+
+    await newTransaction.populate("categoryId", "name type");
   res.json({ message: "Transaction created successfully" ,transactions: newTransaction });
 };
 
 const updateTransaction = async (req, res) => {
   const userId = req.userId;
-  const { name, type } = req.body;
-  const result = await transactionModel.findOneAndUpdate({ userId }, {name, type});
-  if(result.deletedCount === 0){
+    const transactionId = req.params.id;
+  const { title, amount, note } = req.body;
+
+  const result = await transactionModel.findOneAndUpdate(
+      { _id: transactionId, userId },
+      { title, amount, note },
+      { new: true },
+    ).populate("categoryId", "name type");
+
+  if(!result){
     return res.status(404).json({message: "Transaction not found"})
   }
   res.json({
-    message: "Transaction updated successfully"
+    message: "Transaction updated successfully",
+    transaction: result,
   });
 };
 const deleteTransaction = async (req, res) => {
-  const id = req.params.find;
+  const transactionId = req.params.find;
   const userId = req.userId;
-  await transactionModel.findOneAndDelete({ id, userId });
+  await transactionModel.findOneAndDelete({ transactionId, userId });
   res.json({
     message: "Transaction Deleted successfully",
   });
