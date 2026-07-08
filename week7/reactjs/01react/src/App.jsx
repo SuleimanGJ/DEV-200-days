c0bd9ad4176f3f93c539698a55e6a8091411255c
suleiman <suleimanosman1023@gmail.com>
2026-07-08 13:12:29 +0300

    started reactjs and this is initial

---
+++ week7/reactjs/01react/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Get started</h1>
+        </div>
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount(count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+
+      <div className="ticks"></div>
+    </>
+  )
+}
+
+export default App
