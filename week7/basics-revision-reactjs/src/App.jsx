1ec4cfd51f97fb1de255c074d9194b31b8f1084e
suleiman <suleimanosman1023@gmail.com>
2026-07-14 13:15:09 +0300

    add props and make it reuse able component

--- week7/basics-revision-reactjs/src/App.jsx
+++ week7/basics-revision-reactjs/src/App.jsx
@@ -6,12 +6,16 @@ function App() {
   return (
     <div>
       <div style={{ background: "darkblue", height: "100%", padding: 10 }}>
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
+        <ProfileCard
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCZjSYv5j7yKO9TZBsDKeLBqZdl2Pd1vsDztOW_OdV8CGohgdwekFJGDBs&s=10"
+          name="John Doe"
+          bio="Fullstack Developer" description="Lorem ipsum dolor sit amet consectetur adipisicing elit.Temporibus laboriosam itaque magnam"
+        />
+        <ProfileCard
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCZjSYv5j7yKO9TZBsDKeLBqZdl2Pd1vsDztOW_OdV8CGohgdwekFJGDBs&s=10"
+          name="Laray Micheal"
+          bio="Frontend Developer" description="Lorem ipsum dolor sit amet consectetur adipisicing elit.Temporibus laboriosam itaque magnam"
+        />
       </div>
     </div>
   );
