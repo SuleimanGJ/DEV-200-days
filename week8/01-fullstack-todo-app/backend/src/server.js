c8cc33c2c32d5c1f6df7570ee19624ab6a8715db
suleiman <suleimanosman1023@gmail.com>
2026-07-24 18:02:39 +0300

    fixing todo backend in-memory overall

--- week8/01-fullstack-todo-app/backend/src/server.js
+++ week8/01-fullstack-todo-app/backend/src/server.js
@@ -1,38 +1,47 @@
 import express from "express";
+import cors from "cors"
 const app = express();
 import { PORT } from "./config/config.js";
 import { connectToDB } from "./config/db.js";
 
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.json());
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.send("App is working");
 })
 
-let todos = [{
-    id: 1,
-    title: "hello",
-    description: "what are you learning"
-}];
-
+let todos = [];
+// let todos = [{
+//     id: 1,
+//     title: "hello",
+//     description: "what are you learning"
+// }];
+let nextId = 1;
 app.post("/todos", (req, res) => {
     const { title, description } = req.body;
-    const lastId = todos[todos.length - 1].id + 1 || 0;
-    const newTodo = { id: lastId, title, description }
+    // const lastId = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
+    const newTodo = { id: nextId++, title, description }
     todos.push(newTodo);
     res.status(201).json({ success: true, data: newTodo, message: "Todo added successfully" })
 })
 
 
 app.get("/todos", (req, res) => {
-    res.json({ success: true, data: todos, message: "Todo deleted successfully" })
+    res.json({ success: true, data: todos, message: "Todo fetched successfully" })
 })
 
 app.delete("/todos/:id", (req, res) => {
-    const { id } = req.params;
-    const deleteTodo = todos[id];
-    todos = todos.slice(0, id); // use index, have problem of inconsistent id
-    // todos = todos.filter(todo => todo.id !== id)
+    const id = Number(req.params.id);
+    const deleteTodo = todos.find(t => t.id === id);
+    // todos = todos.slice(0, id); // use index, have problem of inconsistent id
+    todos = todos.filter(todo => todo.id !== id)
     res.json({ success: true, data: deleteTodo, message: "Todo deleted successfully" })
 })
 
