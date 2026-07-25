01fd8bec3af99b3a507229a8a861f5259271624b
suleiman <suleimanosman1023@gmail.com>
2026-07-25 07:05:31 +0300

    added usePrev usage

--- week8/custom-hooks/src/App.jsx
+++ week8/custom-hooks/src/App.jsx
@@ -1,19 +1,40 @@
 import { useState } from 'react'
 import './App.css'
-import useFetch from './components/useFetch'
+// import useFetch from './components/useFetch'
+import usePrev from './components/usePrev'
 
+
+// ------------- useFetch --------------------
+// useFetch example
+// function App() {
+
+//   const [data] = useFetch("https://jsonplaceholder.typicode.com/todos");
+
+//   return (
+//     <>
+//       <h1>Hello World!</h1>
+//       {data &&
+//         data.map((item) => {
+//           return <p key={item.id}>{item.title}</p>;
+//         })}
+//     </>
+//   );
+// }
+
+// ------------- usePrev --------------------
+// usePrev example
 function App() {
 
-  const [data] = useFetch("https://jsonplaceholder.typicode.com/todos");
+  const [state, setState] = useState(0)
+  const prevState = usePrev(state);
 
   return (
-    <>
+    <div>
       <h1>Hello World!</h1>
-      {data &&
-        data.map((item) => {
-          return <p key={item.id}>{item.title}</p>;
-        })}
-    </>
+      <h3>Current State Value: {state}</h3>
+      <button onClick={() => setState(s => s + 1)}>Increment</button>
+      <h3>Previous State Value: {prevState}</h3>
+    </div>
   );
 }
 
