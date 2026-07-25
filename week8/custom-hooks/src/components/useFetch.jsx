2cc2fb9b79d97228292c22cf335bd71f2461cfde
suleiman <suleimanosman1023@gmail.com>
2026-07-25 06:41:40 +0300

    added loading inside useFetch hook

--- week8/custom-hooks/src/components/useFetch.jsx
+++ week8/custom-hooks/src/components/useFetch.jsx
@@ -3,33 +3,33 @@ import { useEffect, useState } from "react";
 export default function useFetch(url) {
   console.log(url);
   const [data, setData] = useState(null);
-  // const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(true)
 
   const getData = () => {
     fetch(url)
       .then((res) => res.json())
-      .then((result) => setData(result));
+      .then((result) => {setData(result); setLoading(false);});
   };
   useEffect(() => {
-    // setLoading(true);
     getData();
-    // setLoading(false);
   }, [url]);
 
+//   return {data, loading}
   return [data, loading];
 }
 
 
 // usage of useFetch
 
-  const [data] = useFetch("https://jsonplaceholder.typicode.com/todos");
+//   const [data, loading] = useFetch("https://jsonplaceholder.typicode.com/todos");
 
-  return (
-    <>
-      <h1>Hello World!</h1>
-      {data &&
-        data.map((item) => {
-          return <p key={item.id}>{item.title}</p>;
-        })}
-    </>
-  );
+// if (loading) return <div>Loading...</div>;
+//   return (
+//     <>
+//       <h1>Hello World!</h1>
+//       {data &&
+//         data.map((item) => {
+//           return <p key={item.id}>{item.title}</p>;
+//         })}
+//     </>
+//   );
