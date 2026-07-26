138930b3c15c0788567c3e24bbbbc5a94f7dae80
suleiman <suleimanosman1023@gmail.com>
2026-07-26 09:07:20 +0300

    added another method of usePrev hooka

--- week9/custom-hooks/src/components/usePrev.jsx
+++ week9/custom-hooks/src/components/usePrev.jsx
@@ -1,11 +1,22 @@
 import { useRef, useEffect } from "react";
 
-
-export default function usePrev(value){
-    const ref = useRef()
-    useEffect(() => { ref.current = value }, [value])
-    return ref.current
+export default function usePrev(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+  return ref.current;
 }
 
 // usage
 // const prevCount = usePrevious(count);
+
+// // alternative usePrev
+// function useAlternativePrevious(value, initial) {
+//   const ref = useRef({ target: value, previous: initial });
+//   if (ref.current.target !== value) {
+//     ref.current.previous = ref.current.target;
+//     ref.current.target = value;
+//   }
+//   return ref.current.previous;
+// }
