a8f67f151557eb242f953d6fffe6ecc752829ba4
suleiman <suleimanosman1023@gmail.com>
2026-07-28 11:01:31 +0300

    added todo controllers

---
+++ week8/01-fullstack-todo-app/backend/src/controllers/todoControllers.js
@@ -0,0 +1,25 @@
+import { Todos } from "../models/todoModel";
+
+
+const getAll = async (req, res) => {
+    const todos = await Todos.find({});
+    res.json({ success: true, data: todos, message: "Todo fetched successfully" });
+}
+const createTodo = async (req, res) => {
+    const { title, description } = req.body;
+    const newTodo = await Todos.create({ title, descritption });
+    res.status(201).json({ success: true, data: newTodo, message: "Todo added successfully" });
+}
+const updateTodo = async (req, res) => {
+    const id = req.params.id;
+    const updateTodo = await Todos.findByIdAndUpdate(id, { title: req.body.title, description: req.body.description }, { new: true });
+    res.json({ success: true, data: updateTodo, message: "Todo updated successfully" });
+}
+const deleteTodo = async (req, res) => {
+    const id = req.params.id;
+    const deleteTodo = await Todos.findByIdAndDelete({ id });
+    res.json({ success: true, data: deleteTodo, message: "Todo deleted successfully" });
+}
+
+
+export { getAllTodo, createTodo, deleteTodo}
