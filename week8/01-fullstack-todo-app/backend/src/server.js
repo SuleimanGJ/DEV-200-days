b0f38a3ac0dd16f38ee6b22d5d7970725d52f063
suleiman <suleimanosman1023@gmail.com>
2026-07-28 09:19:41 +0300

    deleted todo logic and will be moved to controllers folder

--- week8/01-fullstack-todo-app/backend/src/server.js
+++ week8/01-fullstack-todo-app/backend/src/server.js
@@ -17,33 +17,6 @@ app.get("/", (req, res) => {
     res.send("App is working");
 })
 
-let todos = [];
-// let todos = [{
-//     id: 1,
-//     title: "hello",
-//     description: "what are you learning"
-// }];
-let nextId = 1;
-app.post("/todos", (req, res) => {
-    const { title, description } = req.body;
-    // const lastId = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
-    const newTodo = { id: nextId++, title, description }
-    todos.push(newTodo);
-    res.status(201).json({ success: true, data: newTodo, message: "Todo added successfully" })
-})
-
-
-app.get("/todos", (req, res) => {
-    res.json({ success: true, data: todos, message: "Todo fetched successfully" })
-})
-
-app.delete("/todos/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const deleteTodo = todos.find(t => t.id === id);
-    // todos = todos.slice(0, id); // use index, have problem of inconsistent id
-    todos = todos.filter(todo => todo.id !== id)
-    res.json({ success: true, data: deleteTodo, message: "Todo deleted successfully" })
-})
 
 connectToDB().then(() => {
     app.listen(PORT, () => {
