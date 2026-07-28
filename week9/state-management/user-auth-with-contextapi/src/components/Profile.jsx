ef3895d8188837ab2f41c207bab68c70306465e2
suleiman <suleimanosman1023@gmail.com>
2026-07-28 06:46:27 +0300

    added form login

--- week9/state-management/user-auth-with-contextapi/src/components/Profile.jsx
+++ week9/state-management/user-auth-with-contextapi/src/components/Profile.jsx
@@ -1,21 +1,41 @@
+import { useState } from "react";
 import { useAuth } from "../hooks/useAuth";
 
 function Login() {
   const { login, user } = useAuth();
-console.log(login)
-  function handleLogin() {
+  const [username, setUsername] = useState("")
+  const [role, setRole] = useState("")
+let genId = 0;
+  function handleLogin(e) {
+    e.preventDefault();
     login({
-      id: 1,
-      name: "Suleiman",
-      role: "admin",
+      id: genId++,
+      name: username,
+      role: role,
     });
+    setUsername("")
   }
 
   return (
     <>
-        <button onClick={handleLogin}>
-        Login
-        </button>
+      <form style={{ display: "flex", flexDirection: "column", maxWidth: "450px" }}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label htmlFor="role">Role</label>
+        <select
+          id="role"
+          value={role}
+          onChange={(e) => setRole(e.target.value)}
+        >
+          <option value="user">User</option>
+          <option value="admin">Admin</option>
+        </select>
+      </form>
+      <button onClick={handleLogin}>Login</button>
     </>
   );
 }
