c3c0f9724e1d3036b0e3a81e7d501cc0591355d6
suleiman <suleimanosman1023@gmail.com>
2026-08-16 10:30:05 +0300

    feat: add send money page

--- week10/paytm/frontend/src/pages/SendMoney.jsx
+++ week10/paytm/frontend/src/pages/SendMoney.jsx
@@ -1,7 +1,40 @@
-function SendMoney() {
+ function SendMoney() {
   return (
-    <div>
-      <h1>SendMoney</h1>
+    <div className="bg-gray-100 flex justify-center h-screen">
+      <div className=" flex flex-col justify-center h-full">
+        <div className="bg-white rounded-lg shadow-lg h-min w-96 max-w-md p-4 space-y-8 text-card-foreground">
+          <div class="flex flex-col space-y-1.5 p-6">
+            <h2 class="text-3xl font-bold text-center">Send Money</h2>
+          </div>
+          <div class="p-6">
+            <div class="flex items-center space-x-4">
+              <div class="w-12 h-12 rounded-full bg-green-500 flex items-center justify-center">
+                <span class="text-2xl text-white">A</span>
+              </div>
+              <h3 class="text-2xl font-semibold">Friend's Name</h3>
+            </div>
+            <div class="space-y-4">
+              <div class="space-y-2">
+                <label
+                  class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  for="amount"
+                >
+                  Amount (in Rs)
+                </label>
+                <input
+                  type="number"
+                  class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm mt-1"
+                  id="amount"
+                  placeholder="Enter amount"
+                />
+              </div>
+              <button class="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white cursor-pointer">
+                Initiate Transfer
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
