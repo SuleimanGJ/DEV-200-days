89f409d1a1475d9f82a5a5a5a674ddf5383d461f
Suleiman <suleimanosman1023@gmail.com>
2026-08-24 06:00:39 +0300

    add create table

---
+++ week11/basics-sql/src/create-table.ts
@@ -0,0 +1,30 @@
+import { getClient } from "./utils.js"
+
+async function createTable() {
+    const client = await getClient();
+
+    const createUsersTableQuery = `
+    CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        possword VARCHAR(255) NOT NULL
+    );
+    `;
+
+    await client.query(createUsersTableQuery);
+
+    const createTodosQuery = `
+    CREATE TABLE todos (
+        id SERIAL PRIMARY KEY,
+        title TEXT NOT NULL,
+        description TEXT,
+        user_id INTEGER REFERENCES users(id),
+        done BOOLEAN DEFAULT FALSE
+    );
+    `;
+
+    await client.query(createTodosQuery);
+    console.log("Table created successfully!");
+}
+
+createTable();
