3dd290025c6a643a2e130e9cc2d224a824e18ba5
Suleiman <suleimanosman1023@gmail.com>
2026-08-26 09:29:51 +0300

    chore: add initial packages and config,setup

---
+++ week11/todo-ts-pg/src/config/database.ts
@@ -0,0 +1,18 @@
+import { Pool } from "pg";
+import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from "./config.js";
+
+// const pool = new Pool({
+//     host: DB_HOST,
+//     port: DB_PORT,
+//     database: DB_DATABASE,
+//     user: DB_USER,
+//     password: DB_PASSWORD
+// });
+
+// or
+
+const pool = new Pool({
+    connectionString: `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
+});
+
+export default pool;
