9b93d4ceedf5d50ad6d905d1737d8984011299f3
Suleiman <suleimanosman1023@gmail.com>
2026-08-26 09:31:34 +0300

    add db name

--- week11/basics-sql/src/utils.ts
+++ week11/basics-sql/src/utils.ts
@@ -1,11 +1,11 @@
 import { Client } from "pg";
-import { DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from "./config.js";
+import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from "./config.js";
 
 
 
 // postgresql://[user]:[password]@[host]:[port]/[database]
 export const getClient = async() => {
-    const client = new Client(`postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`);
+    const client = new Client(`postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`);
     await client.connect();
     return client;
 }
