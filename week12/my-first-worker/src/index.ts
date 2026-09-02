48118e79d1e8560bb3ed4a5ef98e242fdbe07d54
Suleiman <suleimanosman1023@gmail.com>
2026-09-02 07:12:29 +0300

    add serverless my first worker

---
+++ week12/my-first-worker/src/index.ts
@@ -0,0 +1,26 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
+ * `Env` object can be regenerated with `npm run cf-typegen`.
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export interface Env {
+	
+}
+
+export default {
+	async fetch(request, env, ctx): Promise<Response> {
+		// return new Response("Hello World!");
+		// returning json file
+		return Response.json({
+			message: "Hello Wrangler"
+		})
+	},
+} satisfies ExportedHandler<Env>;
