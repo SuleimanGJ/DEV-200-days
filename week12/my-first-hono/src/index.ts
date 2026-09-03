165096f7f658733bee6bbf70b6a3e186c60fc2b3
Suleiman <suleimanosman1023@gmail.com>
2026-09-03 08:12:44 +0300

    add experiment request obj

--- week12/my-first-hono/src/index.ts
+++ week12/my-first-hono/src/index.ts
@@ -3,6 +3,22 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
+  // const body = await c.req.json();
+  // console.log(body)
+  console.log(c.req.header())
+  console.log(c.req.header("Authorization"))
+  console.log(c.req)
+  return c.text('Hello Hono!')
+})
+
+app.post('/', async (c) => {
+  const body = await c.req.json();
+  console.log(body)
+  console.log(c.req.header)
+  console.log(c.req.header("Authorization"))
+  console.log(c.req.query("param"))
+  console.log(c.req.param("name"))
+  console.log(c.req)
   return c.text('Hello Hono!')
 })
 
